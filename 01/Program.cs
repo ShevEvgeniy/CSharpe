47157a74f5f86d58decb83cfb25c2841534a201a
Evgeniy-Shitov <shivev@gmail.com>
2022-09-02 23:06:37 +0300

    task

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,2 +1,10 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число ");
+string input = Console.ReadLine(); // ввод
+
+int number = int.Parse(input); // число
+
+//if (int.Parse (input))
+
+int square = number * number; //квадрат
+
+Console.WriteLine($"Число в квадрате = {square}");
