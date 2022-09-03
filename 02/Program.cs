c63fda01def99ed5e0f5aac4b50f2608fdbabbe4
Evgeniy-Shitov <shivev@gmail.com>
2022-09-03 20:39:32 +0300

    Add a number squared by another number

---
+++ 02/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Введите 2 числа");
+string first = Console.ReadLine();
+string second = Console.ReadLine();
+var num1 = double.Parse(first);
+var num2 = double.Parse(second);
+if (num1*num1==num2)
+{
+    Console.Write($"{num1} квадратный корень числа {num2}");
+}
+else if (num2*num2==num1)
+{
+    Console.Write($"{num2} квадратный корень числа {num1}");
+}
+else
+{
+    Console.WriteLine("числа не являются квадратами друг друга");
+}
