8d3e0a43fdf03ab9a01c9fea82460fbe77f16f15
Evgeniy-Shitov <shivev@gmail.com>
2022-09-03 22:59:14 +0300

    Seminar 1 task2 H

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число a");
+int number1=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int number2=int.Parse(Console.ReadLine());
+if (number1>number2)
+{
+    Console.WriteLine($"Число a = {number1} больше");
+}
+else if (number1<number2)
+{
+    Console.WriteLine($"Число b = {number2} больше");
+}
+else
+{
+    Console.WriteLine("Числа a и b равны");
+}
