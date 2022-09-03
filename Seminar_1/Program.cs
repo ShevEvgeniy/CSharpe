7861e26610de16cb6a0959810ab8150aa76273b9
Evgeniy-Shitov <shivev@gmail.com>
2022-09-03 23:01:16 +0300

    Seminar 1 task2

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Введите число a");
+﻿//Какое число большее?
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое из них большее, а какое меньшее.
+
+
+Console.WriteLine("Введите число a");
 int number1=int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число b");
 int number2=int.Parse(Console.ReadLine());
