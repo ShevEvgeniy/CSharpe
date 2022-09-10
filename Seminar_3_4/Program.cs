5831131317e7a08a6d5c15ed5bd98e11d76a13b2
Evgeniy-Shitov <shivev@gmail.com>
2022-09-10 21:35:35 +0300

    Seminar 3 4

---
+++ Seminar_3_4/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+while (true) // Зацикливание программы
+{
+    Console.WriteLine();
+    Console.WriteLine("Введите число или нажмите *q* для выхода");
+    ConsoleKeyInfo digit = Console.ReadKey(); // в задаче говориться про цифру, а не число, решил сделать через ReadKey
+    Console.WriteLine();
+
+    int iDigit = digit.KeyChar - '0';
+
+    double[] arrey = new double[iDigit + 1];
+    if (iDigit > 0 && iDigit < 10)
+    {
+        /* for (int step = 1; step <= iDigit; step++)
+         {
+             arrey[step] = Math.Pow(step, 2);
+             Console.Write($"{arrey[step]}");
+             if (step < iDigit);
+             Console.Write(", ");
+         }
+         */
+        for (int step = 1; step <= iDigit; step++)
+            arrey[step - 1] = step * step; // поправка step -1, потому что нумерация с 0
+        for (int step = 0; step < iDigit; step++)
+        {
+            Console.Write(arrey[step]);
+            if (step < iDigit);
+            Console.Write(", ");
+        }
+    }
+    else if (digit.KeyChar == 'q') // выход из while
+    {
+        Console.WriteLine("Приходите еще");
+        break;
+    }
+    else { Console.WriteLine("Ошибочка"); }
+}
