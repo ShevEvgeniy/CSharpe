d60bd8ba5f99478fa7a123ca765e841850e7f7b4
Evgeniy-Shitov <shivev@gmail.com>
2022-09-10 18:56:39 +0300

    Seminar 3 2

---
+++ Seminar_3_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y)
+
+Console.WriteLine ("Введите номер четверти ");
+int quarter = int.Parse (Console.ReadLine()!);
+
+if (quarter ==1) {
+    Console.WriteLine ("Х > 0 и Y > 0");
+}
+else if (quarter ==2) {
+    Console.WriteLine ("Х < 0 и Y > 0");
+}
+else if (quarter ==3) {
+    Console.WriteLine ("Х < 0 и Y < 0");
+}
+else if (quarter ==4){
+    Console.WriteLine ("Х > 0 и Y < 0");
+}
+else
+    Console.WriteLine ("Такой четверти не существует");
+
+
