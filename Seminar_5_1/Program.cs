c6e9a05671094f3ea0035fffa6eb103b2ff0798c
Evgeniy-Shitov <shivev@gmail.com>
2022-09-13 21:09:41 +0300

    seminar 5 1

---
+++ Seminar_5_1/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program {
+
+    public static void Main () {
+        const int Size = 12;  // КаждоеСловоСЗаглавнойБуквы    PascalCase
+        int[] numbers = CreateRandomArray (Size);
+        PrintArray (numbers);
+        int positivesSum = CalcPositivesSum (numbers);
+        int negativesSum = CalcNegativesSum (numbers);
+        Console.WriteLine ($"Сумма положительных = {positivesSum}, сумма отрицательных = {negativesSum}");
+    }
+
+    static void PrintArray (int[] numbers) {
+        foreach (int number in numbers)
+            System.Console.Write ($"{number} ");
+    }
+
+    static int[] CreateRandomArray (int size) {
+        int[] numbers = new int[size];
+        for (int i = 0; i < size; i++) {
+            numbers[i] = new Random ().Next (-9, 10);  // правая граница не включается
+        }
+        return numbers;
+    }
+
+    static int CalcPositivesSum (int[] numbers) {
+        int positivesSum = 0;
+        // для каждого числа number из массива чисел numbers
+        foreach (int number in numbers) {
+            if (number > 0)
+                positivesSum += number;
+        }
+        return positivesSum;
+    }
+    static int CalcNegativesSum (int[] numbers) {
+        int negativesSum = 0;
+        foreach (int number in numbers) {
+            if (number < 0)
+                negativesSum += number;
+        }
+        return negativesSum;
+    }
+}
