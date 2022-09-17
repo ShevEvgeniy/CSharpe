62b21e2b33937a1d8e7e64ea595e90d96488bc5e
Evgeniy-Shitov <shivev@gmail.com>
2022-09-17 22:59:03 +0300

    Seminar 5 3 task 32

---
+++ Seminar_5_3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+const int Size = 12;
+
+int[] CreateArray(int size)
+{
+    int[] numbers = new int[size];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-9,10);
+    }
+    return numbers;
+}
+int [] InverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+bool LookForExact(int[] arr, int exact)
+    {
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == exact)
+            return true;
+    }
+    return false;
+    }
+
+    int[] array = CreateArray(Size);
+    PrintArray(array);
+    InverseArray(array);
+    PrintArray(array);
+   
