fc97a637071d6c5772ed96b7c644ed1587b1b12e
Evgeniy-Shitov <shivev@gmail.com>
2022-09-17 18:59:42 +0300

    Seminar 4 5 task 30

---
+++ Seminar_4_5/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+
+Console.WriteLine("Введите N ");
+
+bool numBool = false;
+int num = 0;
+while (!numBool)
+{
+    Console.WriteLine("Число должно быть целым");
+    numBool = int.TryParse(Console.ReadLine()!, out num);
+}
+
+int[] array =new int[num];
+FillArray(array);
+Console.WriteLine("Наш массив случайных чисел, 0 и 1:");
+for (int i = 0; i < num; i++)
+{
+    Console.Write($"{array[i]} ");
+}
