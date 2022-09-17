d767acc4d34f36bbbeb63004ffa2d8ee495875ee
Evgeniy-Shitov <shivev@gmail.com>
2022-09-17 16:52:42 +0300

    Seminar 4 1 task 24

---
+++ Seminar_4_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+Console.WriteLine("Пожалуйста введите число N: ");
+int max = int.Parse(Console.ReadLine()!);
+
+int sum = 0;
+for (int i = 1; i <= max; i++) // 1 2 3 4 5
+    sum = sum + i;
+Console.WriteLine(sum);
