ac165ce24aab8d90c26d6cda7f9e6ea9cc9f7683
Evgeniy-Shitov <shivev@gmail.com>
2022-09-17 16:12:41 +0300

    seminar 3 19 homework

--- Seminar_3_19_homework/Program.cs
+++ Seminar_3_19_homework/Program.cs
@@ -2,7 +2,7 @@
 // 14212 -> нет
 // 23432 -> да
 // 12821 -> да
-
+/*
 Console.Write("Введите число: ");
 string? number = Console.ReadLine();
 
@@ -17,3 +17,27 @@ if (number!.Length == 5){
   CheckNumber(number);
 }
 else Console.WriteLine($"Введи правильное число");
+*/
+Console.Write("Введите число: ");
+string number = Console.ReadLine()!;
+Console.Write(number);
+
+// is, has, глагол в третьем лице goes does
+bool isPalindrome = Palindrom(number);
+
+ string txt = isPalindrome == true? " -> Да" : " -> Нет";
+ Console.WriteLine(txt);
+
+bool Palindrom(string number)
+
+{
+  //bool check = true;
+  for (int i = 0; i < number.Length / 2; i++)
+  {
+    //check = check & number[i] == number[number.Length -i -1];
+    if (number[i] != number[number.Length -i -1])
+    return false;
+  }
+  return true;
+}
+
