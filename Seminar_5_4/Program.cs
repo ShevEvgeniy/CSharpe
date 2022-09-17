b9e1a2d417f689b5d0ba078410689825a1c8915b
Evgeniy-Shitov <shivev@gmail.com>
2022-09-17 23:47:59 +0300

    Seminar 5 4 task 33

---
+++ Seminar_5_4/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+//  4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, -3] -> да
+
+const int Size = 12;
+
+int[] CreateArray(int size)
+{
+    int[] numbers = new int[size];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-6,7);
+    }
+    return numbers;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+bool LookForExact(int[] arr, int exact)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == exact)
+            return true;
+    }
+    return false;
+}
+
+int[] array = CreateArray (Size);
+Console.WriteLine("Введите число");
+int search = int.Parse(Console.ReadLine());
+PrintArray(array);
+if (LookForExact((array), search))
+Console.WriteLine($"В данном массиве искомое число {search} присутствует.");
+else 
+Console.WriteLine($"В данном массиве искомое число {search} отсутствует.");
