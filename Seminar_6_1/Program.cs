42ff456e949864903e7a4be9268ed4339be823e9
Evgeniy-Shitov <shivev@gmail.com>
2022-09-19 00:13:15 +0300

    Seminar 6 1 task 39

---
+++ Seminar_6_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+PrintArray (numbers);
+
+ReverseArray (numbers);
+PrintArray (numbers);
+// int a = 5;
+// int b = 2;
+// Swap (ref a, ref b);
+// System.Console.WriteLine($"{a} {b}");
+
+void Swap (ref int a, ref int b){
+    int c = a;
+    a = b; 
+    b = c;
+}
+
+void PrintArray (int [] nambers) {
+    Console.WriteLine(string.Join (", ", numbers));
+}
+
+void ReverseArray (int[] numbers){
+for (int i =0; i < numbers.Length / 2; i++) {
+    // мы хотим поменять местами 1-й с
+    
+    //int temp = numbers[i];
+    //numbers[i] = numbers[j];
+    //numbers[j] = temp; вместо них используем
+    Swap (ref numbers [i], ref numbers[numbers.Length -1 - i]);
+}
+
+}
