865083f46a20f89428616691d26d0cc7ca9a1398
Evgeniy-Shitov <shivev@gmail.com>
2022-09-18 20:27:32 +0300

    Seminar 5 task homework

---
+++ Seminar_5_34_homework/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Задайте размер массива");
+int size = int.Parse(Console.ReadLine()!);
+int[] numbers = new int[size];
+ArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+void ArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+//{
+    //Console.WriteLine($"[");
+    //Console.WriteLine(string.Join (", ", array));
+    //Console.WriteLine("]"); Преобразуем эти три строчки
+   //----- Console.WriteLine($"[{string.Join (", ", array)}]");
+
+//}
