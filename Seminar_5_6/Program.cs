aa65d565e7692d3282d816ccc1165e223eece184
Evgeniy-Shitov <shivev@gmail.com>
2022-09-18 18:45:52 +0300

    Seminar 5 6 task 37

---
+++ Seminar_5_6/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 9
+//[6 7 3 6] -> 36 21
+
+
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] CreateArray(int size, int smallest, int biggest)
+{
+    int[] numbers = new int[size];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(smallest,biggest+1);
+    }
+    return numbers;
+}
+
+int[] MultiplyPair(int[] arr)
+{
+    int[] multpliedVector = new int[(arr.Length + 1)/2];
+    for (int i = 0; i < (arr.Length +1) /2; i++) 
+    {
+        multpliedVector[i] = arr[i] * arr[arr.Length - i- 1];
+    }
+    return multpliedVector;
+}
+const int Size =20;
+
+int[] array = CreateArray (Size, 1, 100); //опишем функцию так, чтобы числа описывали диапазон включительно
+Console.WriteLine("Было:");
+PrintArray(array);
+Console.WriteLine("Стало:");
+PrintArray(MultiplyPair(array));
